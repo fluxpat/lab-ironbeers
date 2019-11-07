2457e500aa5fb7d7b4a24ac0db32ed8d4d63f60c
Patrick Cho <pck.cho@gmail.com>
2019-11-07 15:48:48 +0100

    incomplete lab

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,21 +1,40 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname + '/views/partials');
 
-
-
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      // console.log(beers);
+      res.render('beers', { beerUnit: beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
 
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom()
+    .then(beers => {
+      console.log(beers);
+      res.render('random-beers', { beerRandom: beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
 
-app.listen(3000);
+app.listen(5000);
